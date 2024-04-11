0964d810f506b505e34bc3538701b6c92d6fede8
Viktor Gubanov <viktor.gubanov@phishman.ru>
2024-04-11 17:35:24 +0300

    Added the ability to configure the service using environment variables

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,13 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	env "github.com/caarlos0/env/v6"
+)
 
 type config struct {
-	RunAddr      string
-	ShortURLBase string
+	RunAddr      string `env:"SERVER_ADDRESS"`
+	ShortURLBase string `env:"BASE_URL"`
 }
 
 var Values config
@@ -14,7 +17,19 @@ func Init() {
 		RunAddr:      ":8080",
 		ShortURLBase: "http://localhost:8080",
 	}
+
 	flag.StringVar(&Values.RunAddr, "a", Values.RunAddr, "address and port to run server")
 	flag.StringVar(&Values.ShortURLBase, "b", Values.ShortURLBase, "base address of the resulting shortened URL")
 	flag.Parse()
+
+	var valuesFromEnv config
+	env.Parse(&valuesFromEnv)
+
+	if valuesFromEnv.RunAddr != "" {
+		Values.RunAddr = valuesFromEnv.RunAddr
+	}
+
+	if valuesFromEnv.ShortURLBase != "" {
+		Values.ShortURLBase = valuesFromEnv.ShortURLBase
+	}
 }
