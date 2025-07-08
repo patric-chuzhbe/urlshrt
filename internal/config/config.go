3e43f2876440a6091541b5da44a2bf1ed61d74a5
Viktor Gubanov <viktor.gubanov@phishman.ru>
2025-07-08 16:34:38 +0300

    Added configuration via JSON file

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,14 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 	"net/url"
 	"os"
+	"reflect"
 	"time"
 
 	"github.com/caarlos0/env/v6"
@@ -18,20 +20,38 @@ import (
 // Config holds the application configuration loaded from environment variables
 // and optionally overridden by command-line flags.
 type Config struct {
-	RunAddr                    string        `env:"SERVER_ADDRESS" envDefault:":8080" validate:"hostname_port"`           // Server address and port (e.g., ":8080")
-	ShortURLBase               string        `env:"BASE_URL" envDefault:"http://localhost:8080" validate:"url"`           // Base URL used to build short URLs
-	LogLevel                   string        `env:"LOG_LEVEL" envDefault:"info" validate:"loglevel"`                      // Logging level (e.g., "info", "debug")
-	DBFileName                 string        `env:"FILE_STORAGE_PATH" envDefault:"" validate:"filepath"`                  // Path to the JSON file storage (used if no DB DSN)
-	DatabaseDSN                string        `env:"DATABASE_DSN"`                                                         // DSN for PostgreSQL database connection
-	DBConnectionTimeout        time.Duration `env:"DB_CONNECTION_TIMEOUT" envDefault:"10s"`                               // Timeout for DB connection attempts
-	AuthCookieName             string        `env:"AUTH_COOKIE_NAME" envDefault:"auth"`                                   // Name of the authentication cookie
-	AuthCookieSigningSecretKey string        `env:"AUTH_COOKIE_SIGNING_SECRET_KEY" envDefault:"LduYtmp2gWSRuyQyRHqbog=="` // Secret key for signing auth cookies
-	ChannelCapacity            int           `env:"CHANNEL_CAPACITY" envDefault:"1024"`                                   // Channel capacity for background jobs
-	DelayBetweenQueueFetches   time.Duration `env:"DELAY_BETWEEN_QUEUE_FETCHES" envDefault:"5s"`                          // Delay between attempts to dequeue jobs
-	MigrationsDir              string        `env:"MIGRATIONS_DIR" envDefault:"migrations"`                               // Directory path for database migration files
-	EnableHTTPS                bool          `env:"ENABLE_HTTPS" envDefault:"false"`
-	CertFile                   string        `env:"CERT_FILE" envDefault:"../../cert/cert.pem"`
-	KeyFile                    string        `env:"KEY_FILE" envDefault:"../../cert/key.pem"`
+	RunAddr                    string        `env:"SERVER_ADDRESS" validate:"hostname_port" json:"server_address"`   // Server address and port (e.g., ":8080")
+	ShortURLBase               string        `env:"BASE_URL" validate:"url" json:"base_url"`                         // Base URL used to build short URLs
+	LogLevel                   string        `env:"LOG_LEVEL"  validate:"loglevel"`                                  // Logging level (e.g., "info", "debug")
+	DBFileName                 string        `env:"FILE_STORAGE_PATH"  validate:"filepath" json:"file_storage_path"` // Path to the JSON file storage (used if no DB DSN)
+	DatabaseDSN                string        `env:"DATABASE_DSN" json:"database_dsn"`                                // DSN for PostgreSQL database connection
+	DBConnectionTimeout        time.Duration `env:"DB_CONNECTION_TIMEOUT"`                                           // Timeout for DB connection attempts
+	AuthCookieName             string        `env:"AUTH_COOKIE_NAME"`                                                // Name of the authentication cookie
+	AuthCookieSigningSecretKey string        `env:"AUTH_COOKIE_SIGNING_SECRET_KEY"`                                  // Secret key for signing auth cookies
+	ChannelCapacity            int           `env:"CHANNEL_CAPACITY"`                                                // Channel capacity for background jobs
+	DelayBetweenQueueFetches   time.Duration `env:"DELAY_BETWEEN_QUEUE_FETCHES"`                                     // Delay between attempts to dequeue jobs
+	MigrationsDir              string        `env:"MIGRATIONS_DIR"`                                                  // Directory path for database migration files
+	EnableHTTPS                bool          `env:"ENABLE_HTTPS"  json:"enable_https"`
+	CertFile                   string        `env:"CERT_FILE"`
+	KeyFile                    string        `env:"KEY_FILE"`
+	JSONConfigFilePath         string        `env:"CONFIG"`
+}
+
+var defaultConfig = Config{
+	RunAddr:                    ":8080",
+	ShortURLBase:               "http://localhost:8080",
+	LogLevel:                   "info",
+	DBFileName:                 "",
+	DBConnectionTimeout:        10 * time.Second,
+	AuthCookieName:             "auth",
+	AuthCookieSigningSecretKey: "LduYtmp2gWSRuyQyRHqbog==",
+	ChannelCapacity:            1024,
+	DelayBetweenQueueFetches:   5 * time.Second,
+	MigrationsDir:              "migrations",
+	EnableHTTPS:                false,
+	CertFile:                   "../../cert/cert.pem",
+	KeyFile:                    "../../cert/key.pem",
+	JSONConfigFilePath:         "config.json",
 }
 
 type initOptions struct {
@@ -48,39 +68,33 @@ func New(optionsProto ...InitOption) (*Config, error) {
 		protoOption(options)
 	}
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Printf("Unable to load .env file: %v", err)
-	}
+	config := &Config{}
 
-	values := Config{}
+	parseJSON(config)
 
-	err = env.Parse(&values)
+	err := parseENV(config)
 	if err != nil {
 		return nil, err
 	}
 
 	if !options.disableFlagsParsing {
-		flag.StringVar(&values.RunAddr, "a", values.RunAddr, "address and port to run server")
-		flag.StringVar(&values.ShortURLBase, "b", values.ShortURLBase, "base address of the resulting shortened URL")
-		flag.StringVar(&values.LogLevel, "l", values.LogLevel, "logger level")
-		flag.StringVar(&values.DBFileName, "f", values.DBFileName, "JSON file name with database")
-		flag.StringVar(&values.DatabaseDSN, "d", values.DatabaseDSN, "A string with the database connection details")
-		flag.BoolVar(&values.EnableHTTPS, "s", values.EnableHTTPS, "HTTPS enabling flag")
-		flag.Parse()
+		parseFlags(config)
 	}
 
-	err = values.clarifyRunAddr()
+	applyDefaults(config, defaultConfig)
+
+	err = config.clarifyRunAddr()
 	if err != nil {
 		return nil, err
 	}
 
-	err = values.clarifyShortURLBase()
+	err = config.clarifyShortURLBase()
 	if err != nil {
 		return nil, err
 	}
 
-	return &values, values.Validate()
+	return config, config.Validate()
+
 }
 
 // Validate validates the configuration struct fields using custom and built-in rules.
@@ -98,6 +112,46 @@ func (conf *Config) Validate() error {
 	}
 
 	return validate.Struct(conf)
+}
+
+func (conf *Config) clarifyRunAddr() error {
+	host, port, err := net.SplitHostPort(conf.RunAddr)
+	if err != nil {
+		return fmt.Errorf("in internal/config/config.go/clarifyRunAddr(): error while `net.SplitHostPort()` calling: %w", err)
+	}
+
+	if conf.EnableHTTPS && (port == "8080" || port == "80") {
+		port = "443"
+	}
+
+	conf.RunAddr = net.JoinHostPort(host, port)
+
+	return nil
+}
+
+func (conf *Config) clarifyShortURLBase() error {
+	if !conf.EnableHTTPS {
+		return nil
+	}
+
+	parsed, err := url.Parse(conf.ShortURLBase)
+	if err != nil {
+		return fmt.Errorf("in internal/config/config.go/clarifyShortURLBase(): error while `url.Parse()` calling: %w", err)
+	}
+
+	host := parsed.Hostname()
+	port := parsed.Port()
+
+	switch port {
+	case "443", "80", "8080":
+		port = ""
+	case "":
+	default:
+		port = ":" + port
+	}
+
+	conf.ShortURLBase = fmt.Sprintf("https://%s%s", host, port)
+	return nil
 }
 
 // InitOption is a functional option type for configuring New().
@@ -131,40 +185,68 @@ func validateLogLevel(fieldLevel validator.FieldLevel) bool {
 	return allowedLogLevels[value]
 }
 
-func (conf *Config) clarifyRunAddr() error {
-	host, port, err := net.SplitHostPort(conf.RunAddr)
+func applyDefaults(config *Config, defaultConfig Config) {
+	vCfg := reflect.ValueOf(config).Elem()
+	vDef := reflect.ValueOf(defaultConfig)
+
+	for i := 0; i < vCfg.NumField(); i++ {
+		field := vCfg.Field(i)
+		defVal := vDef.Field(i)
+
+		if field.CanSet() && field.IsZero() {
+			field.Set(defVal)
+		}
+	}
+}
+
+func parseFlags(config *Config) {
+	flag.StringVar(&config.RunAddr, "a", config.RunAddr, "address and port to run server")
+	flag.StringVar(&config.ShortURLBase, "b", config.ShortURLBase, "base address of the resulting shortened URL")
+	flag.StringVar(&config.LogLevel, "l", config.LogLevel, "logger level")
+	flag.StringVar(&config.DBFileName, "f", config.DBFileName, "JSON file name with database")
+	flag.StringVar(&config.DatabaseDSN, "d", config.DatabaseDSN, "A string with the database connection details")
+	flag.BoolVar(&config.EnableHTTPS, "s", config.EnableHTTPS, "HTTPS enabling flag")
+
+	JSONConfigFilePathDesc := "JSON configuration file path"
+	flag.StringVar(&config.JSONConfigFilePath, "c", config.JSONConfigFilePath, JSONConfigFilePathDesc)
+	flag.StringVar(&config.JSONConfigFilePath, "config", config.JSONConfigFilePath, JSONConfigFilePathDesc)
+
+	flag.Parse()
+}
+
+func parseENV(config *Config) error {
+	err := godotenv.Load()
 	if err != nil {
-		return fmt.Errorf("in internal/config/config.go/clarifyRunAddr(): error while `net.SplitHostPort()` calling: %w", err)
+		log.Printf("Unable to load .env file: %v", err)
 	}
 
-	if conf.EnableHTTPS && (port == "8080" || port == "80") {
-		port = "443"
+	err = env.Parse(config)
+	if err != nil {
+		return err
 	}
-
-	conf.RunAddr = net.JoinHostPort(host, port)
 
 	return nil
 }
 
-func (conf *Config) clarifyShortURLBase() error {
-	if !conf.EnableHTTPS {
-		return nil
+func parseJSON(values *Config) {
+	var configFileName string
+	if val := os.Getenv("CONFIG"); val != "" {
+		configFileName = val
+	}
+	for i, arg := range os.Args {
+		if (arg == "-c" || arg == "-config") && i+1 < len(os.Args) {
+			configFileName = os.Args[i+1]
+		}
 	}
 
-	parsed, err := url.Parse(conf.ShortURLBase)
-	if err != nil {
-		return fmt.Errorf("in internal/config/config.go/clarifyShortURLBase(): error while `url.Parse()` calling: %w", err)
+	if file, err := os.Open(configFileName); err == nil {
+		defer file.Close()
+		if err := json.NewDecoder(file).Decode(values); err != nil {
+			log.Printf("Failed to parse %s: %v", configFileName, err)
+		} else {
+			log.Printf("Loaded config from %s", configFileName)
+		}
+	} else if !os.IsNotExist(err) {
+		log.Printf("Failed to open %s: %v", configFileName, err)
 	}
-	host, port, err := net.SplitHostPort(parsed.Host)
-	if err != nil {
-		return fmt.Errorf("in internal/config/config.go/clarifyShortURLBase(): error while `net.SplitHostPort()` calling: %w", err)
-	}
-	if port == "443" || port == "80" || port == "8080" {
-		port = ""
-	} else {
-		port = fmt.Sprintf(":%s", port)
-	}
-	conf.ShortURLBase = fmt.Sprintf("https://%s%s", host, port)
-
-	return nil
 }
--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"os"
 	"testing"
 
 	"github.com/caarlos0/env/v6"
@@ -11,27 +12,25 @@ import (
 func TestClarifyShortURLBaseDisableHttps(t *testing.T) {
 	values := Config{}
 
-	err := env.Parse(&values)
+	applyDefaults(&values, defaultConfig)
+
+	err := values.clarifyShortURLBase()
 	require.NoError(t, err)
 
-	err = values.clarifyShortURLBase()
-	require.NoError(t, err)
-
-	assert.Equal(t, values.ShortURLBase, "http://localhost:8080")
+	assert.Equal(t, "http://localhost:8080", values.ShortURLBase)
 }
 
 func TestClarifyShortURLBaseEnableHttps(t *testing.T) {
 	values := Config{}
 
-	err := env.Parse(&values)
-	require.NoError(t, err)
+	applyDefaults(&values, defaultConfig)
 
 	values.EnableHTTPS = true
 
-	err = values.clarifyShortURLBase()
+	err := values.clarifyShortURLBase()
 	require.NoError(t, err)
 
-	assert.Equal(t, values.ShortURLBase, "https://localhost")
+	assert.Equal(t, "https://localhost", values.ShortURLBase)
 }
 
 func TestClarifyShortURLBaseEnableHttpsAltPort(t *testing.T) {
@@ -48,4 +47,87 @@ func TestClarifyShortURLBaseEnableHttpsAltPort(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Equal(t, values.ShortURLBase, "https://localhost:447")
+}
+
+const testJSON = `{
+	"server_address": ":3000",
+	"base_url": "http://json-config.com",
+	"file_storage_path": "json_storage.json",
+	"database_dsn": "json-dsn",
+	"enable_https": true
+}`
+
+func writeTempJSON(t *testing.T, content string) string {
+	t.Helper()
+	file, err := os.CreateTemp("", "config*.json")
+	require.NoError(t, err)
+	_, err = file.WriteString(content)
+	require.NoError(t, err)
+	require.NoError(t, file.Close())
+	t.Cleanup(func() {
+		err := os.Remove(file.Name())
+		require.NoError(t, err)
+	})
+	return file.Name()
+}
+
+func TestConfigPriorityJSONOnly(t *testing.T) {
+	jsonPath := writeTempJSON(t, testJSON)
+	t.Setenv("CONFIG", jsonPath)
+
+	cfg, err := New(WithDisableFlagsParsing(true))
+	require.NoError(t, err)
+
+	assert.Equal(t, ":3000", cfg.RunAddr)
+	assert.Equal(t, "https://json-config.com", cfg.ShortURLBase)
+	assert.Equal(t, "json_storage.json", cfg.DBFileName)
+	assert.Equal(t, "json-dsn", cfg.DatabaseDSN)
+	assert.True(t, cfg.EnableHTTPS)
+}
+
+func TestConfigPriorityJSONPlusEnv(t *testing.T) {
+	jsonPath := writeTempJSON(t, testJSON)
+	t.Setenv("CONFIG", jsonPath)
+	t.Setenv("SERVER_ADDRESS", ":4000")
+	t.Setenv("BASE_URL", "http://env.com")
+
+	cfg, err := New(WithDisableFlagsParsing(true))
+	require.NoError(t, err)
+
+	assert.Equal(t, ":4000", cfg.RunAddr) // env overrides json
+	assert.Equal(t, "https://env.com", cfg.ShortURLBase)
+	assert.Equal(t, "json-dsn", cfg.DatabaseDSN) // from JSON
+}
+
+func TestConfigPriorityAllSources(t *testing.T) {
+	jsonPath := writeTempJSON(t, testJSON)
+	t.Setenv("CONFIG", jsonPath)
+	t.Setenv("SERVER_ADDRESS", ":4000")
+	t.Setenv("BASE_URL", "http://env.com")
+
+	os.Args = []string{
+		"testbin",
+		"-a", ":6000",
+		"-b", "http://cli.com",
+	}
+
+	cfg, err := New()
+	require.NoError(t, err)
+
+	assert.Equal(t, ":6000", cfg.RunAddr) // CLI > ENV > JSON
+	assert.Equal(t, "https://cli.com", cfg.ShortURLBase)
+	assert.Equal(t, "json-dsn", cfg.DatabaseDSN) // from JSON
+}
+
+func TestConfigEnvOnly(t *testing.T) {
+	t.Setenv("SERVER_ADDRESS", ":7000")
+	t.Setenv("BASE_URL", "http://envonly.com")
+	t.Setenv("LOG_LEVEL", "debug")
+
+	cfg, err := New(WithDisableFlagsParsing(true))
+	require.NoError(t, err)
+
+	assert.Equal(t, ":7000", cfg.RunAddr)
+	assert.Equal(t, "http://envonly.com", cfg.ShortURLBase)
+	assert.Equal(t, "debug", cfg.LogLevel)
 }
